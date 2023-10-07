62d1b7f060bcac671b8773eaa764bd0d98e987b8
zuidec <case@CASE-DESKTOP>
2023-10-07 13:57:15 -0700

    Test app can connect to and read from MPU then output on UART

--- testapps/i2c_test.c
+++ testapps/i2c_test.c
@@ -2,6 +2,8 @@
 #include <libopencm3/cm3/scb.h>
 #include <libopencm3/stm32/gpio.h>
 #include <string.h>
+#include <stdio.h>
+
 #include "core/system.h"
 #include "drivers/uart/uart.h"
 #include "drivers/i2c/i2c.h"
@@ -23,7 +25,8 @@
 
 #define MPU_ADDR            (0x68)
 i2c_handle_t i2c1   ={0U};
-uint8_t buffer[128] = {0};
+volatile uint8_t buffer[128] = {'\0'};
+volatile uint8_t output[128] = {'\0'};
 fifo_buffer_t fifo;
 static void gpio_setup(void)    {
     
@@ -38,7 +41,11 @@ static void gpio_setup(void)    {
     gpio_set_af(UART2_PORT,GPIO_AF7, TX2_PIN | RX2_PIN );
     
 }
-
+static void buffer_reset(uint8_t* data, uint16_t size)  {
+    for(uint16_t i =0; i< size; i++)    {
+        data[i] = '\0';
+    }
+}
 int main(void)  {
     gpio_setup();
     system_setup();
@@ -46,18 +53,117 @@ int main(void)  {
     
     fifo_buffer_setup(&fifo, buffer, 128);
     i2c_setup(&i2c1, (uint32_t)I2C1_BASE, (uint32_t)I2C1_PORT, (uint32_t)I2C1_SDA, (uint32_t)I2C1_SCL, &fifo);
-    volatile uint8_t data = 0;
-    uart_write_byte(122);
+    volatile uint8_t data = 1;
+
+    uart_write("\nBoot complete\n",15);
+    uart_write("Resetting MPU\n",14);
+    system_delay(100);
+
+    i2c_write_register(&i2c1, (1<<7), MPU_ADDR, MPU60X0_REG_PWR_MGMT_1);
+    i2c_write_register(&i2c1, 0x07, MPU_ADDR, MPU60X0_REG_SIGNAL_PATH_RESET);
+    system_delay(100);
+    i2c_write_register(&i2c1, 0x07, MPU_ADDR, MPU60X0_REG_USER_CTRL);
+    system_delay(100);
+    i2c_write_register(&i2c1, 0x00, MPU_ADDR, MPU60X0_REG_PWR_MGMT_1);
+    i2c_write_register(&i2c1, 0x02, MPU_ADDR, MPU60X0_REG_CONFIG);
+    i2c_write_register(&i2c1, 0x08, MPU_ADDR, MPU60X0_REG_SMPLRT_DIV);
+    
+    uart_println("Verifying settings");
+    //uart_write("Verifying settings\n",19);
+    
+    volatile uint8_t reg = 0;
+    reg = i2c_read_register(&i2c1,MPU_ADDR,MPU60X0_REG_PWR_MGMT_1);    
+    if(reg != 0x00)    {
+        uart_println("PWR_MGMT_1 FAIL");
+        while(data){data++;}
+    }
+
+    reg = i2c_read_register(&i2c1,MPU_ADDR,MPU60X0_REG_SIGNAL_PATH_RESET);    
+    if(reg !=0x00)    {
+        uart_println("SIGNAL_PATH_RESET FAIL");
+        while(data){data++;}
+    }
+
+    reg = i2c_read_register(&i2c1,MPU_ADDR,MPU60X0_REG_USER_CTRL);    
+    if(reg !=0x00)    {
+        uart_println("USER_CTRL FAIL");
+        while(data){data++;}
+    }
+
+    reg = i2c_read_register(&i2c1,MPU_ADDR,MPU60X0_REG_CONFIG);    
+    if(reg !=0x02)    {
+        uart_println("CONFIG FAIL");
+        while(data){data++;}
+    }
+
+    reg = i2c_read_register(&i2c1,MPU_ADDR,MPU60X0_REG_SMPLRT_DIV);    
+    if(reg !=0x08)    {
+        uart_println("SMPLRT_DIV FAIL");
+        while(data){data++;}
+    }
+
+    uart_println("Settings verified");
     gpio_toggle(LED_PORT, LED_PIN);
     uint64_t millis = system_get_ticks();
-    
+    volatile int16_t value = 0;
+    volatile uint16_t buf_size = 0;
     while(1)    {
       //  
-        if(system_get_ticks()-millis >=2000) {
+        if(system_get_ticks()-millis >=100) {
             millis = system_get_ticks();
             gpio_toggle(LED_PORT, LED_PIN);
             
-            data = i2c_read_byte(&i2c1, MPU_ADDR, MPU60X0_REG_PWR_MGMT_1);
+            
+            i2c_read_registers_burst(&i2c1,&buffer,6,MPU_ADDR, MPU60X0_REG_ACCEL_XOUT_H);
+            value = (int16_t)((buffer[0] << 8) | buffer[1]);
+            value = value*16384;
+            buf_size = sprintf(output, "Accel x: %8i",value);
+            uart_write(output, buf_size);
+            buffer_reset(&output[0], 128);//memset(&output[0], 0, 128);
+            
+            value = (int16_t)((buffer[2] << 8) | buffer[3]);
+            value = value*16384;
+            buf_size = sprintf(output, " Accel y: %8i",value);
+            uart_write(output, buf_size);
+            buffer_reset(&output[0], 128);//memset(&output[0], 0, 128);
+            
+            value = (int16_t)((buffer[4] << 8) | buffer[5]);
+            value = value*16384;
+            buf_size = sprintf(output, " Accel z: %8i",value);
+            uart_write(output, buf_size);
+            buffer_reset(&output[0], 128);//memset(&output[0], 0, 128);
+            buffer_reset(&buffer[0], 128);//memset(&buffer[0], 0, 128);
+
+            i2c_read_registers_burst(&i2c1,&buffer,6,MPU_ADDR, MPU60X0_REG_ACCEL_XOUT_H);
+            value = (int16_t)((buffer[0] << 8) | buffer[1]);
+            value = value/131;
+            buf_size = sprintf(output, " Gyro x: %8i",value);
+            uart_write(output, buf_size);
+            buffer_reset(&output[0], 128);//memset(&output[0], 0, 128);
+            
+            value = (int16_t)((buffer[2] << 8) | buffer[3]);
+            value = value/131;
+            buf_size = sprintf(output, " Gyro y: %8i",value);
+            uart_write(output, buf_size);
+            buffer_reset(&output[0], 128);//memset(&output[0], 0, 128);
+            
+            value = (int16_t)((buffer[4] << 8) | buffer[5]);
+            value = value/131;
+            buf_size = sprintf(output, " Gyro z: %8i \n",value);
+            uart_write(output, buf_size);
+            buffer_reset(&output[0], 128);//memset(&output[0], 0, 128);
+            buffer_reset(&buffer[0], 128);uart_write_16(value);//memset(&buffer[0], 0, 128);
+            
+            /*
+            uart_write(" gyro y: ",9);
+            uart_write_16(value);
+            uart_write(" gyro z: ",9);
+            uart_write_16(value);
+            uart_write_byte('\n');
+            */
+           // memset(&buffer, 0, 128);
+            
+            /*data = i2c_read_byte(&i2c1, MPU_ADDR, MPU60X0_REG_PWR_MGMT_1);
             uart_write_byte(data);
             uart_write_byte(0x20);
             data = i2c_read_byte(&i2c1, MPU_ADDR, MPU60X0_REG_WHO_AM_I);
@@ -73,7 +179,7 @@ int main(void)  {
             data = i2c_read_byte(&i2c1, MPU_ADDR, MPU60X0_REG_PWR_MGMT_1);
             uart_write_byte(data);
             uart_write_byte(0x0A);
-            /*uart_write_byte(0x00);
+            uart_write_byte(0x00);
             uart_write_byte(0x3A);
             uart_write_byte(0x20);
             uart_write_byte(0x20);
