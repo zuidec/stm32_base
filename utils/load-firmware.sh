33a4ee6a408d76a320487db36afce1596b865dca
zuidec <case@CASE-DEKSTOP>
2023-11-07 22:03:33 -0800

    Added usage/help statement

--- utils/load-firmware.sh
+++ utils/load-firmware.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-while getopts f: flag
+function usage() {
+
+        echo "Usage: $0 [-f firmware] -h "
+        echo ""
+        echo "      -f [firmware_file]  Firmware file to be loaded to board,"
+        echo "                          can be in .bin or .elf format"
+        echo ""
+        echo "      -h                  Show this help message"
+}
+
+while getopts "hf:" flag
 do
         case "${flag}" in
+                h) usage
+                exit 0;;
                 f) filename=${OPTARG};;
+                \?) usage
+                exit 1;;
         esac
 done
 
-echo "Writing $filename to ST-Link...\n"
+echo "Writing $filename to ST-Link..."
 
 sudo st-info --probe
 sudo st-flash write $filename 0x80000000
